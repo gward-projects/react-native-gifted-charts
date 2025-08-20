d7182ea83849eb61bf9df02c55d925766e354f8a
gward-projects <services@gward.fr>
2025-08-20 16:09:54 +0200

    fix: missing argument

--- src/BarChart/index.tsx
+++ src/BarChart/index.tsx
@@ -183,6 +183,7 @@ export const BarChart = (props: BarChartPropsType) => {
       width: totalWidth,
       screenWidth,
       containsNegative: false,
+      containerHeightIncludingBelowXAxis
     });
   };
 
